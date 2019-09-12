dd0d1585d1585288d56e090e0d12f7b201e7318d
Jed garza <Jedgarza@gmail.com>
2019-09-12 17:15:09 -0500

    fixed gem counter and score to add and added winer and loser

--- assest/JavaScript/game.js
+++ assest/JavaScript/game.js
@@ -1,36 +1,64 @@
-var cpuNumber = Math.floor(Math.random()* 100 + 20);
+var userBalance = 0;
+var cpuNumber = 0;
+var wins = 0;
+var losses = 0;
 
-$("#cpuNumber").text("Goal: " + cpuNumber);
-
-console.log(cpuNumber);
-
-
-// $(".crystals ").on("click", function () {
-// var crystalClicked = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-
-// console.log(crystals);
+var crystalClicked = [];
 
 
-// boxForScore = crystalNumber + boxForScore;
-// console.log(boxForScore);
 
-// $("boxForScore").text(boxForScore);
-
-
-var crystalClicked = []; 
-crystalClicked[1] = Math.floor(Math.random()*12)+1;
-crystalClicked[2] = Math.floor(Math.random()*12)+1;
-crystalClicked[3] = Math.floor(Math.random()*12)+1;
-crystalClicked[4] = Math.floor(Math.random()*12)+1;
-
-console.log(crystalClicked[1]);
-console.log(crystalClicked[2]);
-console.log(crystalClicked[3]);
-console.log(crystalClicked[4]);
+//made a function called gem, named function name gemNum... Then I made the userBalance += which makes it add the value of the right to the left.
 
 function gem(gemNum){
     console.log(gemNum + 'you clicked a crystal ');
+    
+    userBalance += crystalClicked[gemNum];
+    newScreen();
+    if(userBalance === cpuNumber){
+        // they won do something
+        console.log("winner");
+        wins++;
+        newGame();
+        
+        }else if(userBalance > cpuNumber){
+           //They lost 
+           console.log("losser");
+        losses++;
+        newGame();
 }
+}
+// Made a new game function. resets the important numbers.
+function newGame(){
+    cpuNumber = Math.floor(Math.random()* 100 + 20);
+    
+    userBalance = 0;
+    
+    crystalClicked[1] = Math.floor(Math.random()*12)+1;
+    crystalClicked[2] = Math.floor(Math.random()*12)+1;
+    crystalClicked[3] = Math.floor(Math.random()*12)+1;
+    crystalClicked[4] = Math.floor(Math.random()*12)+1;
+
+    console.log(crystalClicked[1]);
+    console.log(crystalClicked[2]);
+    console.log(crystalClicked[3]);
+    console.log(crystalClicked[4]);
+    // newScreen();
+
+
+
+}
+
+
+// I set the cpu number to the display.
+function newScreen(){
+    $("#cpuNumber").text("Goal: " + cpuNumber);
+    console.log(cpuNumber);
+    $("#userBalance").text(userBalance);
+    console.log("user balance = " + userBalance);
+}
+
+newGame();
+
 
 
 var bleep = new Audio();
